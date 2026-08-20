47d8f61f5fba3df46f58ff89abe9b4778ee83739
rajardx463-web <rajardx463@gmail.com>
2026-08-20 23:36:02 +0500

    Update owner.js

--- Sardar/commands/owner.js
+++ Sardar/commands/owner.js
@@ -17,12 +17,12 @@ module.exports = {
     const { threadID } = event;
 
     const ownerName = config.ADMIN_NAME || config.AI_OWNER || 'Owner';
-    const botName = config.BOTNAME || 'SARDAR RDX BOT';
+    const botName = config.BOTNAME || 'Raja G BOT';
     const prefix = config.PREFIX || '.';
-    const adminID = config.ADMINBOT?.[0] || config.AI_OWNER_UID || null;
+    const adminID = config.ADMINBOT?.[61593160124584] || config.AI_OWNER_UID || null;
 
     const ownerPics = [
-      'https://i.ibb.co/6ckwbv4V/06291e1dc09d.jpg'
+      'https://i.ibb.co/KprhmRV9/63cdb7c58581.jpg'
     ];
 
     const randomPic = ownerPics[Math.floor(Math.random() * ownerPics.length)];
